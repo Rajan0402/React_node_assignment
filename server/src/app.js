4629c928865ced29efd4090a53113e90185dc844
{Rajan0402} <rajanyadav2873@gmail.com>
2025-07-15 13:50:38 +0530

    init

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import user from "./routes/user.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["GET", "POST"],
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome!" });
+});
+
+app.use("/api/user", user);
+
+export default app;
---
+++ server/src/routes/user.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+router
+  .route("/")
+  .get(async (req, res) => {})
+  .post(async (req, res) => {});
+
+export default router;
