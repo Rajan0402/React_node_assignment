7e396a0623d27d7f39f1ec84e958f977eede43a9
{Rajan0402} <rajanyadav2873@gmail.com>
2025-07-15 14:37:45 +0530

    /api/user GET and POST route

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,9 +1,44 @@
 import express from "express";
 const router = express.Router();
+import path from "path";
+import { fileURLToPath } from "url";
+import { promises as fs } from "fs";
+// import usersDb from "../db/usersDb.json" assert { type: "json" };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+let users = JSON.parse(
+  await fs.readFile(path.join(__dirname, "..", "db", "usersDb.json"), "utf-8")
+);
+
+const usersDb = {
+  users,
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
 
 router
   .route("/")
-  .get(async (req, res) => {})
-  .post(async (req, res) => {});
+  .get(async (req, res) => {
+    const latestUser = usersDb.users[-1];
+    res.send(latestUser);
+  })
+  .post(async (req, res) => {
+    try {
+      const { firstName, lastName, dob } = req.body;
+      const newUser = { firstName, lastName, dob };
+      usersDb.setUsers([...usersDb.users, newUser]);
+      console.log(usersDb);
+      await fs.writeFile(
+        path.join(__dirname, "..", "db", "usersDb.json"),
+        JSON.stringify(usersDb.users)
+      );
+      res.send({ message: "User Added!" });
+    } catch (error) {
+      console.log("error: ", error);
+      res.send({ error });
+    }
+  });
 
 export default router;
